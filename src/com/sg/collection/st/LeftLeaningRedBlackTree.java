a00b099a70a760dbf082a3ff3990cb7b4230ba49
theodoregao <theodore.gao.1986@gmail.com>
2021-02-15 22:49:57 -0800

    Implement LeftLeaningRedBlackTree.

---
+++ src/com/sg/collection/st/LeftLeaningRedBlackTree.java
@@ -0,0 +1,334 @@
+package com.sg.collection.st;
+
+import com.sg.collection.queue.LinkedListQueue;
+import com.sg.collection.queue.Queue;
+
+import java.util.Iterator;
+
+public class LeftLeaningRedBlackTree<Key extends Comparable<Key>, Value> implements OrderST<Key, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+    private Node root;
+
+    public LeftLeaningRedBlackTree() {
+        root = null;
+    }
+
+    @Override
+    public void put(Key key, Value value) {
+        if (key == null) return;
+        if (value == null) {
+            delete(key);
+        } else {
+            root = put(root, key, value);
+            root.color = BLACK;
+        }
+    }
+
+    @Override
+    public Value get(Key key) {
+        if (key == null) return null;
+        final Node node = get(root, key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException();
+        if (!contains(key)) return;
+        if (!isRed(root.left) && isRed(root.right)) root.color = RED;
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    @Override
+    public boolean contains(Key key) {
+        return key != null && get(root, key) != null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    @Override
+    public Key min() {
+        final Node node = min(root);
+        return node == null ? null : node.key;
+    }
+
+    @Override
+    public Key max() {
+        final Node node = max(root);
+        return node == null ? null : node.key;
+    }
+
+    @Override
+    public Key floor(Key key) {
+        if (key == null) return null;
+        final Node node = floor(root, key);
+        return node == null ? null : node.key;
+    }
+
+    @Override
+    public Key ceiling(Key key) {
+        if (key == null) return null;
+        final Node node = ceiling(root, key);
+        return node == null ? null : node.key;
+    }
+
+    @Override
+    public int rank(Key key) {
+        if (key == null) return -1;
+        return rank(root, key);
+    }
+
+    @Override
+    public Key select(int k) {
+        if (k < 0 || k >= size(root)) throw new IllegalArgumentException();
+        final Node node = select(root, k);
+        return node == null ? null : node.key;
+    }
+
+    @Override
+    public void deleteMin() {
+        if (isEmpty()) throw new IllegalStateException();
+        if (!isRed(root.left) && !isRed(root.right)) root.color = RED;
+        root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    @Override
+    public void deleteMax() {
+        if (isEmpty()) throw new IllegalStateException();
+        if (!isRed(root.left) && !isRed(root.right)) root.color = RED;
+        root = deleteMax(root);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    @Override
+    public int size(Key lo, Key hi) {
+        final Iterator<Key> it = keys(lo, hi).iterator();
+        int size = 0;
+        while (it.hasNext()) {
+            size++;
+            it.next();
+        }
+        return size;
+    }
+
+    @Override
+    public Iterable<Key> keys(Key lo, Key hi) {
+        final Queue<Key> queue = new LinkedListQueue<>();
+        inOrderIterator(root, queue, lo, hi);
+        return queue;
+    }
+
+    @Override
+    public Iterable<Key> keys() {
+        if (size() == 0) return new LinkedListQueue<>();
+        return keys(min(), max());
+    }
+
+    private Node get(Node parent, Key key) {
+        if (parent == null) return null;
+        final int compare = key.compareTo(parent.key);
+        if (compare == 0) return parent;
+        else if (compare < 0) return get(parent.left, key);
+        else return get(parent.right, key);
+    }
+
+    private Node put(Node parent, Key key, Value value) {
+        Node node = parent;
+        if (node == null) node = new Node(key, value, RED);
+        final int compare = key.compareTo(node.key);
+        if (compare == 0) node.value = value;
+        else if (compare < 0) node.left = put(node.left, key, value);
+        else node.right = put(node.right, key, value);
+
+        return balance(node);
+    }
+
+    private int size(Node parent) {
+        if (parent == null) return 0;
+        return parent.size;
+    }
+
+    private Node min(Node parent) {
+        if (parent == null) return null;
+        else if (parent.left == null) return parent;
+        else return min(parent.left);
+    }
+
+    private Node max(Node parent) {
+        if (parent == null) return null;
+        else if (parent.right == null) return parent;
+        else return max(parent.right);
+    }
+
+    private Node floor(Node parent, Key key) {
+        if (parent == null) return null;
+        final int compare = key.compareTo(parent.key);
+        if (compare == 0) return parent;
+        else if (compare < 0) return floor(parent.left, key);
+        else {
+            final Node rightCandidate = floor(parent.right, key);
+            return rightCandidate == null ? parent : rightCandidate;
+        }
+    }
+
+    private Node ceiling(Node parent, Key key) {
+        if (parent == null) return null;
+        final int compare = parent.key.compareTo(key);
+        if (compare == 0) return parent;
+        else if (compare < 0) return ceiling(parent.right, key);
+        else {
+            final Node leftCandidate = ceiling(parent.left, key);
+            return leftCandidate == null ? parent : leftCandidate;
+        }
+    }
+
+    private int rank(Node parent, Key key) {
+        if (parent == null) return 0;
+        final int compare = key.compareTo(parent.key);
+        if (compare == 0) return size(parent.left);
+        else if (compare < 0) return rank(parent.left, key);
+        else return 1 + size(parent.left) + rank(parent.right, key);
+    }
+
+    private Node select(Node parent, int k) {
+        if (parent == null) return null;
+        if (size(parent.left) == k) return parent;
+        else if (size(parent.left) > k) return select(parent.left, k);
+        else return select(parent.right, k - size(parent.left) - 1);
+    }
+
+    private Node deleteMin(Node parent) {
+        if (parent.left == null) return null;
+        if (!isRed(parent.left) && !isRed(parent.right)) parent = moveRedLeft(parent);
+        parent.left = deleteMin(parent.left);
+        return balance(parent);
+    }
+
+    private Node deleteMax(Node parent) {
+        if (isRed(parent.left)) parent = rotateRight(parent);
+        if (parent.right == null) return null;
+        if (!isRed(parent.right) && !isRed(parent.right.left)) parent = moveRedRight(parent);
+        parent.right = deleteMax(parent.right);
+        return balance(parent);
+    }
+
+    private Node delete(Node parent, Key key) {
+        if (parent == null) return null;
+        if (key.compareTo(parent.key) < 0) {
+            if (!isRed(parent.left) && !isRed(parent.left.left)) parent = moveRedLeft(parent);
+            parent.left = delete(parent.left, key);
+        } else {
+            if (isRed(parent.left)) parent = rotateRight(parent);
+            if (key.compareTo(parent.key) == 0 && parent.right == null) return null;
+            if (!isRed(parent.right) && !isRed(parent.right.left)) parent = moveRedRight(parent);
+            if (key.compareTo(parent.key) == 0) {
+                final Node rightMin = min(parent.right);
+                rightMin.right = deleteMin(parent.right);
+                rightMin.left = parent.left;
+                parent = rightMin;
+            } else {
+                parent.right = delete(parent.right, key);
+            }
+        }
+        return balance(parent);
+    }
+
+    private void inOrderIterator(Node parent, Queue<Key> queue, Key lo, Key hi) {
+        if (parent == null) return;
+        inOrderIterator(parent.left, queue, lo, hi);
+        if (lo.compareTo(parent.key) <= 0 && parent.key.compareTo(hi) <= 0) {
+            queue.enqueue(parent.key);
+        }
+        inOrderIterator(parent.right, queue, lo, hi);
+    }
+
+    private boolean isRed(Node node) {
+        return node != null && node.color;
+    }
+
+    private Node rotateLeft(Node node) {
+        final Node x = node.right;
+        node.right = x.left;
+        x.left = node;
+        x.color = x.left.color;
+        x.left.color = RED;
+        node.calculateSize();
+        x.calculateSize();
+        return x;
+    }
+
+    private Node rotateRight(Node node) {
+        Node x = node.left;
+        node.left = x.right;
+        x.right = node;
+        x.color = x.right.color;
+        x.right.color = RED;
+        node.calculateSize();
+        x.calculateSize();
+        return x;
+    }
+
+    private Node flipColor(Node node) {
+        node.color = !node.color;
+        node.left.color = !node.left.color;
+        node.right.color = !node.right.color;
+        return node;
+    }
+
+    private Node balance(Node node) {
+        if (isRed(node.right) && !isRed(node.left)) node = rotateLeft(node);
+        if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
+        if (isRed(node.left) && isRed(node.right)) flipColor(node);
+        node.calculateSize();
+        return node;
+    }
+
+    private Node moveRedLeft(Node node) {
+        flipColor(node);
+        if (isRed(node.right.left)) {
+            node.right = rotateRight(node.right);
+            node = rotateLeft(node);
+            flipColor(node);
+        }
+        return node;
+    }
+
+    private Node moveRedRight(Node node) {
+        flipColor(node);
+        if (isRed(node.left.left)) {
+            node = rotateRight(node);
+            flipColor(node);
+        }
+        return node;
+    }
+
+    private class Node {
+        final Key key;
+        Value value;
+        Node left;
+        Node right;
+        int size;
+        boolean color;
+
+        Node(Key key, Value value, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+        }
+
+        void calculateSize() {
+            size = size(left) + size(right) + 1;
+        }
+    }
+}
---
+++ test/com/sg/collection/st/LeftLeaningRedBlackTreeTest.java
@@ -0,0 +1,362 @@
+package com.sg.collection.st;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeftLeaningRedBlackTreeTest {
+
+    private OrderST<Character, Integer> st;
+
+    @BeforeEach
+    void setUp() {
+        st = new LeftLeaningRedBlackTree<>();
+    }
+
+    @Test
+    void testFillWithItemsAndDeleteThem_thenCallIsEmpty_correctResultReturned() {
+        assertTrue(st.isEmpty());
+        st.put('a', 1);
+        assertFalse(st.isEmpty());
+        st.delete('a');
+        assertTrue(st.isEmpty());
+
+        st.put('a', 1);
+        assertFalse(st.isEmpty());
+        st.put('b', 2);
+        assertFalse(st.isEmpty());
+        st.put('a', 3);
+        assertFalse(st.isEmpty());
+        st.delete('b');
+        assertFalse(st.isEmpty());
+        st.delete('a');
+        assertTrue(st.isEmpty());
+
+        st.put(null, 1);
+        assertTrue(st.isEmpty());
+
+        st.put('a', 1);
+        assertFalse(st.isEmpty());
+        st.put('a', null);
+        assertTrue(st.isEmpty());
+    }
+
+    @Test
+    void testFillWithItemsAndDeleteThem_thenCallContains_correctResultReturned() {
+        assertFalse(st.contains(null));
+        assertFalse(st.contains('a'));
+        st.put('a', 1);
+        assertTrue(st.contains('a'));
+        assertFalse(st.contains(null));
+        st.delete('a');
+        assertFalse(st.contains('a'));
+
+        st.put('a', 1);
+        assertTrue(st.contains('a'));
+        st.put('b', 2);
+        assertTrue(st.contains('b'));
+        st.put('a', 3);
+        assertTrue(st.contains('a'));
+        st.delete('b');
+        assertFalse(st.contains('b'));
+        st.delete('a');
+        assertFalse(st.contains('a'));
+
+        st.put(null, 1);
+        assertFalse(st.contains(null));
+
+        st.put('a', 1);
+        assertTrue(st.contains('a'));
+        st.put('a', null);
+        assertFalse(st.contains('a'));
+    }
+
+    @Test
+    void testFillWithItemsAndDeleteThem_thenCallSize_correctResultReturned() {
+        assertEquals(0, st.size());
+        st.put(null, 1);
+        assertEquals(0, st.size());
+
+        st.put('a', 1);
+        assertEquals(1, st.size());
+        st.put('b', 2);
+        assertEquals(2, st.size());
+        st.put('a', 0);
+        assertEquals(2, st.size());
+        st.put(null, 3);
+        assertEquals(2, st.size());
+        st.put('a', null);
+        assertEquals(1, st.size());
+        st.delete('a');
+        assertEquals(1, st.size());
+        st.delete('b');
+        assertEquals(0, st.size());
+    }
+
+    @Test
+    void testWithGetPut_thenCorrectValueReturned() {
+        assertNull(st.get('a'));
+        st.put('a', 1);
+        assertEquals(1, st.get('a'));
+        assertNull(st.get('b'));
+        assertNull(st.get(null));
+
+        st.put('b', 2);
+        assertEquals(2, st.get('b'));
+
+        st.put('a', 3);
+        assertEquals(3, st.get('a'));
+
+        st.put('b', null);
+        assertNull(st.get('b'));
+        assertEquals(3, st.get('a'));
+
+        st.delete('a');
+        assertNull(st.get('a'));
+    }
+
+    @Test
+    void testInputWithCharacters_thenIterateWithAllKeys_correctOccurrenceCountReturned() {
+        final String str = "I love China, and I came to United State 10 years ago. Now we are missing home!";
+        for (char ch : str.toCharArray()) {
+            final char key = Character.toLowerCase(ch);
+            if (st.contains(key)) {
+                st.put(key, st.get(key) + 1);
+            } else {
+                st.put(key, 1);
+            }
+        }
+        assertEquals(16, st.get(' '));
+        assertEquals(6, st.get('i'));
+
+        int size = 0;
+        for (Character ch : st.keys()) {
+            size++;
+        }
+        assertEquals(size, st.size());
+    }
+
+    @Test
+    void testPutDifferentValues_thenCallMinMaxFloorCeilingRankSelect_thenCorrectValuesReturned() {
+        assertNull(st.min());
+        assertNull(st.max());
+        assertNull(st.floor(null));
+        assertNull(st.floor('\255'));
+        assertNull(st.ceiling(null));
+        assertNull(st.ceiling('\0'));
+        assertEquals(-1, st.rank(null));
+        assertEquals(0, st.rank('\0'));
+        assertEquals(0, st.rank('\255'));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(-1);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(0);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(1);
+        });
+
+        st.put('b', 1);
+        assertEquals('b', st.min());
+        assertEquals('b', st.max());
+        assertNull(st.floor(null));
+        assertNull(st.floor('a'));
+        assertEquals('b', st.floor('b'));
+        assertEquals('b', st.floor('c'));
+        assertEquals('b', st.floor('x'));
+        assertEquals('b', st.floor('y'));
+        assertEquals('b', st.floor('z'));
+        assertEquals('b', st.floor('\255'));
+        assertNull(st.ceiling(null));
+        assertEquals('b', st.ceiling('\0'));
+        assertEquals('b', st.ceiling('a'));
+        assertEquals('b', st.ceiling('b'));
+        assertNull(st.ceiling('c'));
+        assertNull(st.ceiling('x'));
+        assertNull(st.ceiling('y'));
+        assertNull(st.ceiling('\255'));
+        assertEquals(-1, st.rank(null));
+        assertEquals(0, st.rank('\0'));
+        assertEquals(0, st.rank('a'));
+        assertEquals(0, st.rank('b'));
+        assertEquals(1, st.rank('c'));
+        assertEquals(1, st.rank('x'));
+        assertEquals(1, st.rank('y'));
+        assertEquals(1, st.rank('z'));
+        assertEquals(1, st.rank('\255'));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(-1);
+        });
+        assertEquals('b', st.select(0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(1);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(2);
+        });
+
+        st.put('y', 10);
+        assertEquals('b', st.min());
+        assertEquals('y', st.max());
+        assertNull(st.floor(null));
+        assertNull(st.floor('a'));
+        assertEquals('b', st.floor('b'));
+        assertEquals('b', st.floor('c'));
+        assertEquals('b', st.floor('x'));
+        assertEquals('y', st.floor('y'));
+        assertEquals('y', st.floor('z'));
+        assertEquals('y', st.floor('\255'));
+        assertNull(st.ceiling(null));
+        assertEquals('b', st.ceiling('\0'));
+        assertEquals('b', st.ceiling('a'));
+        assertEquals('b', st.ceiling('b'));
+        assertEquals('y', st.ceiling('c'));
+        assertEquals('y', st.ceiling('x'));
+        assertEquals('y', st.ceiling('y'));
+        assertNull(st.ceiling('\255'));
+        assertEquals(-1, st.rank(null));
+        assertEquals(0, st.rank('\0'));
+        assertEquals(0, st.rank('a'));
+        assertEquals(0, st.rank('b'));
+        assertEquals(1, st.rank('c'));
+        assertEquals(1, st.rank('x'));
+        assertEquals(1, st.rank('y'));
+        assertEquals(2, st.rank('z'));
+        assertEquals(2, st.rank('\255'));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(-1);
+        });
+        assertEquals('b', st.select(0));
+        assertEquals('y', st.select(1));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(2);
+        });
+
+        st.delete('y');
+        assertEquals('b', st.min());
+        assertEquals('b', st.max());
+        assertNull(st.floor(null));
+        assertNull(st.floor('a'));
+        assertEquals('b', st.floor('b'));
+        assertEquals('b', st.floor('c'));
+        assertEquals('b', st.floor('x'));
+        assertEquals('b', st.floor('y'));
+        assertEquals('b', st.floor('z'));
+        assertEquals('b', st.floor('\255'));
+        assertNull(st.ceiling(null));
+        assertEquals('b', st.ceiling('\0'));
+        assertEquals('b', st.ceiling('a'));
+        assertEquals('b', st.ceiling('b'));
+        assertNull(st.ceiling('c'));
+        assertNull(st.ceiling('x'));
+        assertNull(st.ceiling('y'));
+        assertNull(st.ceiling('\255'));
+        assertEquals(-1, st.rank(null));
+        assertEquals(0, st.rank('\0'));
+        assertEquals(0, st.rank('a'));
+        assertEquals(0, st.rank('b'));
+        assertEquals(1, st.rank('c'));
+        assertEquals(1, st.rank('x'));
+        assertEquals(1, st.rank('y'));
+        assertEquals(1, st.rank('z'));
+        assertEquals(1, st.rank('\255'));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(-1);
+        });
+        assertEquals('b', st.select(0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(1);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            st.select(2);
+        });
+
+        st.put('a', 0);
+        st.put('b', 1);
+        st.put('c', 2);
+        st.put('d', 3);
+        st.put('e', 4);
+        st.put('f', 5);
+        st.put('g', 6);
+        assertEquals('f', st.select(5));
+    }
+
+    @Test
+    void testPutDifferentValues_thenCallDeleteMinDeleteMax_thenCorrectValuesReturned() {
+        final String randomAtoZ = "eaiugdqsctyowvhbzxnmkflrpj";
+        for (char ch : randomAtoZ.toCharArray()) {
+            st.put(ch, (int) ch);
+        }
+        assertEquals('a', st.min());
+        assertEquals('z', st.max());
+
+        st.deleteMin();
+        assertEquals('b', st.min());
+
+        st.deleteMax();
+        assertEquals('y', st.max());
+
+        st.deleteMin();
+        assertEquals('c', st.min());
+
+        st.deleteMax();
+        assertEquals('x', st.max());
+
+        st.deleteMin();
+        assertEquals('d', st.min());
+
+        st.deleteMax();
+        assertEquals('w', st.max());
+
+        st.deleteMin();
+        assertEquals('e', st.min());
+
+        st.deleteMax();
+        assertEquals('v', st.max());
+
+        st.deleteMin();
+        assertEquals('f', st.min());
+
+        st.deleteMax();
+        assertEquals('u', st.max());
+
+        st.deleteMin();
+        assertEquals('g', st.min());
+
+        st.deleteMax();
+        assertEquals('t', st.max());
+    }
+
+    @Test
+    void testPutDifferentValues_thenCallKeys_correctValuesReturned() {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            st.put(ch, (int) ch);
+        }
+
+        final char startCh = 'f';
+        final char endCh = 'k';
+        int index = 0;
+        for (char ch : st.keys(startCh, endCh)) {
+            assertEquals(startCh + index++, ch);
+        }
+        assertEquals(6, index);
+        assertEquals(10, st.size('f', 'o'));
+    }
+
+    @Test
+    void testDeleteLogicWithDifferentCases() {
+        st.put('g', 0);
+        st.put('o', 0);
+        st.put('l', 0);
+        st.delete('o');
+        assertEquals('g', st.min());
+        assertEquals('l', st.max());
+
+        st.put('a', 0);
+        st.delete('g');
+        assertEquals('a', st.min());
+    }
+
+}
